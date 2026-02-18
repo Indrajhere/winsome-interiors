e19426e386dd4b0e4a0b424f2088a98bc97d8279
Indrajbir Singh <101132675+Indrajhere@users.noreply.github.com>
2026-02-18 11:14:46 +0530

    Update RequestQuoteBtn.jsx

--- components/RequestQuoteBtn.jsx
+++ components/RequestQuoteBtn.jsx
@@ -6,7 +6,7 @@ import { useRouter } from 'next/navigation';
 
 
 const RequestQuoteBtn = ({ inverted, onClick = null }) => {
-  const {router} = useRouter();
+  const router = useRouter();
 
   const handleClick = () => {
     if (onClick) {
